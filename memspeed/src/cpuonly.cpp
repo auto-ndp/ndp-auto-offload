6c445f66ee4ad5f0d6e9713970ea3549741e5185
Jakub Szewczyk <git@kubasz.xyz>
2021-09-01 16:08:25 +0100

    Test force-remapping written-to memcpy region (it's slow)

--- memspeed/src/cpuonly.cpp
+++ memspeed/src/cpuonly.cpp
@@ -36,9 +36,10 @@ BENCHMARK(BM_cpu_memcopy)->RangeMultiplier(2)->Range(2, 1 * 1024 * 1024 * 1024);
 
 static void BM_cpu_wasmmemloop(benchmark::State &state) {
   size_t g4 = 1024 * 1024 * 1024; // uint32_t takes 4 bytes -> 4GB
-  size_t len_arg = size_t(state.range(2));
   int madv_arg = int(state.range(0));
   bool thp_arg = int(state.range(1)) == 1;
+  bool mmapfixed_arg = int(state.range(2)) == 1;
+  size_t len_arg = size_t(state.range(3));
   MmapArray<uint32_t> src{g4, false};
   if (thp_arg) {
     madvise(src.data(), src.size_in_bytes(), MADV_HUGEPAGE);
@@ -61,6 +62,15 @@ static void BM_cpu_wasmmemloop(benchmark::State &state) {
   for (auto _ : state) {
     {
       MmapArray<uint32_t> dst{g4, false};
+      if (mmapfixed_arg) {
+        void *mm =
+            mmap(dst.data(), 4 * len_arg, PROT_READ | PROT_WRITE,
+                 MAP_FIXED | MAP_ANONYMOUS | MAP_PRIVATE | MAP_POPULATE, -1, 0);
+        if (mm != dst.data()) {
+          perror("Couldn't rewrite page mappings");
+          exit(1);
+        }
+      }
       if (thp_arg) {
         madvise(dst.data(), dst.size_in_bytes(), MADV_HUGEPAGE);
       }
@@ -81,7 +91,7 @@ static void BM_cpu_wasmmemloop(benchmark::State &state) {
 }
 BENCHMARK(BM_cpu_wasmmemloop)
     ->RangeMultiplier(2)
-    ->Ranges({{0, 2}, {0, 1}, {1024, 1 * 1024 * 1024 * 1024}});
+    ->Ranges({{0, 2}, {0, 1}, {0, 1}, {1024, 1 * 1024 * 1024 * 1024}});
 
 static void BM_cpu_memread_linear(benchmark::State &state) {
   size_t len_arg = size_t(state.range(0));
