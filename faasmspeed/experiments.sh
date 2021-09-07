3cc42351a93974b6e8e13f83d2ccb14a41ba6a2e
Jakub Szewczyk <git@kubasz.xyz>
2021-09-07 18:40:22 +0100

    Small fix to terminal output of faasmspeed

--- faasmspeed/experiments.sh
+++ faasmspeed/experiments.sh
@@ -8,11 +8,11 @@ printf "" > $OUTFILE
 RPS_LIST="1 10 20 30 40 50 60 70 80 90 100 110 120 130 140"
 TIME_PER=10
 FAASMSPEED=faasmspeed
-FAASMSPEED=echo
+#FAASMSPEED=echo
 
 for RPS in ${RPS_LIST}
 do
-    echo '*** Hello RPS=${RPS}'
+    echo "*** Hello RPS=${RPS}"
     # Warm-up burst
     ${FAASMSPEED} -u demo -f hello '' -c -t 1 -r ${RPS} -p 2 2>&1 > /dev/null
     sleep 0.2
@@ -22,7 +22,7 @@ sleep 1
 
 for RPS in ${RPS_LIST}
 do
-    echo '*** Wordcount RPS=${RPS}'
+    echo "*** Wordcount RPS=${RPS}"
     # Warm-up burst
     ${FAASMSPEED} -u ndp -f wordcount frankenmod.txt -c -t 1 -r ${RPS} -p 2 2>&1 > /dev/null
     sleep 0.2
@@ -32,7 +32,7 @@ sleep 1
 
 for RPS in ${RPS_LIST}
 do
-    echo '*** Wordcount-NDP RPS=${RPS}'
+    echo "*** Wordcount-NDP RPS=${RPS}"
     # Warm-up burst
     ${FAASMSPEED} -u ndp -f wordcount_manual_ndp frankenmod.txt -c -t 1 -r ${RPS} -p 2 2>&1 > /dev/null
     sleep 0.2
