1b651dccbaef2c2420102021178d6c4c46a2232b
Jakub Szewczyk <git@kubasz.xyz>
2021-09-08 12:07:48 +0100

    typo, forgot the $

--- faasmspeed/experiments.sh
+++ faasmspeed/experiments.sh
@@ -12,23 +12,23 @@ PARALLELISM=1
 TIMEOUT=25
 #FAASMSPEED=echo
 
-# for RPS in ${RPS_LIST}
-# do
-#     echo "*** Hello RPS=${RPS}"
-#     # Warm-up burst
-#     ${FAASMSPEED} -u demo -f hello '' -c -x {TIMEOUT} -t 1 -r ${RPS} -p ${PARALLELISM} 2>&1 > /dev/null
-#     sleep 0.2
-#     ${FAASMSPEED} -u demo -f hello '' -c -x {TIMEOUT} -t ${TIME_PER} -r ${RPS} -p ${PARALLELISM} >> $OUTFILE
-# done
-# sleep 1
+for RPS in ${RPS_LIST}
+do
+    echo "*** Hello RPS=${RPS}"
+    # Warm-up burst
+    ${FAASMSPEED} -u demo -f hello '' -c -x ${TIMEOUT} -t 1 -r ${RPS} -p ${PARALLELISM} 2>&1 > /dev/null
+    sleep 0.2
+    ${FAASMSPEED} -u demo -f hello '' -c -x ${TIMEOUT} -t ${TIME_PER} -r ${RPS} -p ${PARALLELISM} >> $OUTFILE
+done
+sleep 1
 
 for RPS in ${RPS_LIST}
 do
     echo "*** Wordcount RPS=${RPS}"
     # Warm-up burst
-    ${FAASMSPEED} -u ndp -f wordcount frankenmod.txt -c -x {TIMEOUT} -t 1 -r ${RPS} -p ${PARALLELISM} 2>&1 > /dev/null
+    ${FAASMSPEED} -u ndp -f wordcount frankenmod.txt -c -x ${TIMEOUT} -t 1 -r ${RPS} -p ${PARALLELISM} 2>&1 > /dev/null
     sleep 0.2
-    ${FAASMSPEED} -u ndp -f wordcount frankenmod.txt -c -x {TIMEOUT} -t ${TIME_PER} -r ${RPS} -p ${PARALLELISM} >> $OUTFILE
+    ${FAASMSPEED} -u ndp -f wordcount frankenmod.txt -c -x ${TIMEOUT} -t ${TIME_PER} -r ${RPS} -p ${PARALLELISM} >> $OUTFILE
 done
 sleep 1
 
@@ -36,7 +36,7 @@ for RPS in ${RPS_LIST}
 do
     echo "*** Wordcount-NDP RPS=${RPS}"
     # Warm-up burst
-    ${FAASMSPEED} -u ndp -f wordcount_manual_ndp frankenmod.txt -c -x {TIMEOUT} -t 1 -r ${RPS} -p ${PARALLELISM} 2>&1 > /dev/null
+    ${FAASMSPEED} -u ndp -f wordcount_manual_ndp frankenmod.txt -c -x ${TIMEOUT} -t 1 -r ${RPS} -p ${PARALLELISM} 2>&1 > /dev/null
     sleep 0.2
-    ${FAASMSPEED} -u ndp -f wordcount_manual_ndp frankenmod.txt -c -x {TIMEOUT} -t ${TIME_PER} -r ${RPS} -p ${PARALLELISM} >> $OUTFILE
+    ${FAASMSPEED} -u ndp -f wordcount_manual_ndp frankenmod.txt -c -x ${TIMEOUT} -t ${TIME_PER} -r ${RPS} -p ${PARALLELISM} >> $OUTFILE
 done
