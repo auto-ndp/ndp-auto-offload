9f5fb01c47888339366d14bcf23c5e77aba1710a
Jakub Szewczyk <git@kubasz.xyz>
2021-09-13 16:49:28 +0100

    Update experiments.sh

--- faasmspeed/experiments.sh
+++ faasmspeed/experiments.sh
@@ -5,31 +5,33 @@ OUTFILE=results-$(date -I).log
 echo Writing results to $OUTFILE
 printf "" > $OUTFILE
 
-RPS_LIST="1 10 20 30 40 50 60 70 80 90 100 120 140 160 180 200 250 300 400 500 600 700 800 900 1000 1200 1400 1600 1800 2000 2200 2400 2600 2800 3000"
+RPS_LIST="1 10 20 30 40 50 60 70 80 90 100 120 140 160 180 200 250 300 310 320 330 340 350 360 370 380 390 400 410 420 430 440 450 460 470 480 490 500 520 540 560 600 700 800 900 1000 1200 1400 1600"
 TIME_PER=10
 FAASMSPEED=faasmspeed
 PARALLELISM=1
+WARMUP_TIME=2
 TIMEOUT=25000
 HOST=http://127.0.0.1:8080
+MONITOR_HOSTS='localhost:8125;luna:8125'
 #FAASMSPEED=echo
 
-for RPS in ${RPS_LIST}
-do
-    echo "*** Hello RPS=${RPS}"
-    # Warm-up burst
-    ${FAASMSPEED} -h ${HOST} -u demo -f hello '' -c -x ${TIMEOUT} -t 1 -r ${RPS} -p ${PARALLELISM} 2>&1 > /dev/null
-    sleep 0.2
-    ${FAASMSPEED} -h ${HOST} -u demo -f hello '' -c -x ${TIMEOUT} -t ${TIME_PER} -r ${RPS} -p ${PARALLELISM} >> $OUTFILE
-done
-sleep 1
+# for RPS in ${RPS_LIST}
+# do
+#     echo "*** Hello RPS=${RPS}"
+#     # Warm-up burst
+#     ${FAASMSPEED} -h ${HOST} -u demo -f hello '' -c -x ${TIMEOUT} -t 1 -r ${RPS} -p ${PARALLELISM} 2>&1 > /dev/null
+#     sleep 0.2
+#     ${FAASMSPEED} -h ${HOST} -u demo -f hello '' -c -x ${TIMEOUT} -t ${TIME_PER} -r ${RPS} -p ${PARALLELISM} >> $OUTFILE
+# done
+# sleep 1
 
 for RPS in ${RPS_LIST}
 do
     echo "*** Wordcount RPS=${RPS}"
     # Warm-up burst
-    ${FAASMSPEED} -h ${HOST} -u ndp -f wordcount frankenmod.txt -c -x ${TIMEOUT} -t 1 -r ${RPS} -p ${PARALLELISM} 2>&1 > /dev/null
+    ${FAASMSPEED} -h ${HOST} -u ndp -f wordcount frankenmod.txt -c -x ${TIMEOUT} -t ${WARMUP_TIME} -r ${RPS} -p ${PARALLELISM} 2>&1 > /dev/null
     sleep 0.2
-    ${FAASMSPEED} -h ${HOST} -u ndp -f wordcount frankenmod.txt -c -x ${TIMEOUT} -t ${TIME_PER} -r ${RPS} -p ${PARALLELISM} >> $OUTFILE
+    ${FAASMSPEED} -h ${HOST} -u ndp -f wordcount frankenmod.txt -c -m ${MONITOR_HOSTS} -x ${TIMEOUT} -t ${TIME_PER} -r ${RPS} -p ${PARALLELISM} >> $OUTFILE
 done
 sleep 1
 
@@ -37,7 +39,7 @@ for RPS in ${RPS_LIST}
 do
     echo "*** Wordcount-NDP RPS=${RPS}"
     # Warm-up burst
-    ${FAASMSPEED} -h ${HOST} -u ndp -f wordcount_manual_ndp frankenmod.txt -c -x ${TIMEOUT} -t 1 -r ${RPS} -p ${PARALLELISM} 2>&1 > /dev/null
+    ${FAASMSPEED} -h ${HOST} -u ndp -f wordcount_manual_ndp frankenmod.txt -c -x ${TIMEOUT} -t ${WARMUP_TIME} -r ${RPS} -p ${PARALLELISM} 2>&1 > /dev/null
     sleep 0.2
-    ${FAASMSPEED} -h ${HOST} -u ndp -f wordcount_manual_ndp frankenmod.txt -c -x ${TIMEOUT} -t ${TIME_PER} -r ${RPS} -p ${PARALLELISM} >> $OUTFILE
+    ${FAASMSPEED} -h ${HOST} -u ndp -f wordcount_manual_ndp frankenmod.txt -c -m ${MONITOR_HOSTS} -x ${TIMEOUT} -t ${TIME_PER} -r ${RPS} -p ${PARALLELISM} >> $OUTFILE
 done
