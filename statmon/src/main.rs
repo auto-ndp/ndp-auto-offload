a91e433505c25bf4e1ce6e5cfb2379ca34f61293
Jakub Szewczyk <git@kubasz.xyz>
2021-09-13 14:51:05 +0100

    statmon tcp statistics monitor

---
+++ statmon/src/main.rs
@@ -0,0 +1,99 @@
+use anyhow::Result;
+use argh::FromArgs;
+use parking_lot::lock_api::RawMutex;
+use std::time::Duration;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+mod stats;
+use crate::stats::Stats;
+
+#[derive(FromArgs, Debug, Clone)]
+/// Monitoring resource usage over a TCP socket
+struct Options {
+    /// port to listen on
+    #[argh(option, short = 'p', default = "8125")]
+    port: u16,
+}
+
+static STATS: parking_lot::Mutex<Stats> =
+    parking_lot::Mutex::const_new(parking_lot::RawMutex::INIT, Stats::new());
+
+async fn conn_handler(mut conn: tokio::net::TcpStream) -> Result<()> {
+    conn.set_nodelay(true)?;
+    let (rc, mut wc) = conn.split();
+    let mut rc = tokio::io::BufReader::new(rc);
+    let mut rdbuf = [0u8];
+    loop {
+        let _bytes_read = rc.read_exact(&mut rdbuf).await?;
+        match rdbuf[0] {
+            b'\n' | b'\r' | b'\0' => continue,
+            b'q' => break,
+            b'[' => {
+                let mut st = STATS.lock();
+                st.update()?;
+                st.begin_range();
+            }
+            b']' => {
+                let stats = {
+                    let mut st = STATS.lock();
+                    st.update()?;
+                    st.end_range();
+                    st.stat_packet()
+                };
+                wc.write_all(stats.as_bytes()).await?;
+            }
+            b'?' => {
+                let stats = STATS.lock().stat_packet();
+                wc.write_all(stats.as_bytes()).await?;
+            }
+            u => return Err(anyhow::anyhow!("Unknown command byte {}", u)),
+        }
+    }
+    wc.shutdown().await?;
+    Ok(())
+}
+
+async fn async_main(opts: &'static Options) -> Result<()> {
+    STATS.lock().update()?;
+    let sock = tokio::net::TcpListener::bind(std::net::SocketAddrV4::new(
+        std::net::Ipv4Addr::new(0, 0, 0, 0),
+        opts.port,
+    ))
+    .await?;
+    loop {
+        tokio::spawn(async move {
+            let mut interval = tokio::time::interval(Duration::from_millis(250));
+            loop {
+                interval.tick().await;
+                if let Err(e) = STATS.lock().update() {
+                    eprintln!("Couldn't update statistics: {}", e);
+                    panic!("{}", e);
+                }
+            }
+        });
+        match sock.accept().await {
+            Ok((conn, addr)) => {
+                tokio::spawn(async move {
+                    if let Err(e) = conn_handler(conn).await {
+                        eprintln!("Error handling connection from {}: {}", addr, e);
+                    }
+                });
+            }
+            Err(e) => {
+                eprintln!("Error accepting connection: {}", e)
+            }
+        }
+    }
+}
+
+fn main() -> Result<()> {
+    let opts: &'static Options = Box::leak(Box::new(argh::from_env()));
+    eprintln!("[status] Parsed options");
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_time()
+        .enable_io()
+        .build()?;
+    rt.block_on(async_main(opts))?;
+    rt.shutdown_timeout(Duration::from_secs(5));
+    Ok(())
+}
---
+++ statmon/src/stats.rs
@@ -0,0 +1,318 @@
+use anyhow::{Context, Result};
+use std::fmt::Write as _;
+use std::fs::{File, OpenOptions};
+use std::io::{prelude::*, SeekFrom};
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum StatValue {
+    Float(f64),
+    Int(i64),
+}
+
+impl StatValue {
+    pub fn setf(&mut self, v: f64) {
+        *self = Self::Float(v);
+    }
+
+    pub fn seti(&mut self, v: i64) {
+        *self = Self::Int(v);
+    }
+
+    pub fn valf(&self) -> f64 {
+        match self {
+            Self::Float(v) => *v,
+            Self::Int(v) => *v as f64,
+        }
+    }
+
+    pub const fn minval(&self) -> Self {
+        match self {
+            Self::Float(_) => Self::Float(f64::MIN),
+            Self::Int(_) => Self::Int(i64::MIN),
+        }
+    }
+
+    pub const fn maxval(&self) -> Self {
+        match self {
+            Self::Float(_) => Self::Float(f64::MAX),
+            Self::Int(_) => Self::Int(i64::MAX),
+        }
+    }
+
+    pub const fn zeroval(&self) -> Self {
+        match self {
+            Self::Float(_) => Self::Float(0.0),
+            Self::Int(_) => Self::Int(0),
+        }
+    }
+
+    pub fn set_min(&mut self, other: Self) {
+        match (*self, other) {
+            (Self::Float(m), Self::Float(v)) => self.setf(m.min(v)),
+            (Self::Int(m), Self::Int(v)) => self.seti(m.min(v)),
+            _ => panic!("Mismatched statistic types"),
+        }
+    }
+
+    pub fn set_max(&mut self, other: Self) {
+        match (*self, other) {
+            (Self::Float(m), Self::Float(v)) => self.setf(m.max(v)),
+            (Self::Int(m), Self::Int(v)) => self.seti(m.max(v)),
+            _ => panic!("Mismatched statistic types"),
+        }
+    }
+
+    pub fn set_add(&mut self, other: Self) {
+        match (*self, other) {
+            (Self::Float(m), Self::Float(v)) => self.setf(m + v),
+            (Self::Int(m), Self::Int(v)) => self.seti(m + v),
+            _ => panic!("Mismatched statistic types"),
+        }
+    }
+}
+
+impl std::fmt::Display for StatValue {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Float(v) => v.fmt(f),
+            Self::Int(v) => v.fmt(f),
+        }
+    }
+}
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum WhichStat {
+    LoadAvg1min = 0,
+    MemTotal,
+    MemAvailable,
+    MemActive,
+    MemInactive,
+    MemAnonHugePages,
+    //
+    TotalStatCount,
+}
+
+#[derive(Clone, Debug)]
+pub struct Stat {
+    pub name: &'static str,
+    pub value_now: StatValue,
+    pub value_min: StatValue,
+    pub value_max: StatValue,
+    pub value_sum: StatValue,
+    pub value_cnt: i64,
+}
+
+impl Stat {
+    const fn int(name: &'static str) -> Self {
+        Self {
+            name,
+            value_now: StatValue::Int(0),
+            value_min: StatValue::Int(0).maxval(),
+            value_max: StatValue::Int(0).minval(),
+            value_sum: StatValue::Int(0),
+            value_cnt: 0,
+        }
+    }
+
+    const fn float(name: &'static str) -> Self {
+        Self {
+            name,
+            value_now: StatValue::Float(0.0),
+            value_min: StatValue::Float(0.0).maxval(),
+            value_max: StatValue::Float(0.0).minval(),
+            value_sum: StatValue::Float(0.0),
+            value_cnt: 0,
+        }
+    }
+
+    pub fn reset(&mut self) {
+        self.value_min = self.value_now.maxval();
+        self.value_max = self.value_now.minval();
+        self.value_sum = self.value_now.zeroval();
+        self.value_cnt = 0;
+    }
+
+    pub fn update_aggregate(&mut self) {
+        self.value_min.set_min(self.value_now);
+        self.value_max.set_max(self.value_now);
+        self.value_sum.set_add(self.value_now);
+        self.value_cnt += 1;
+    }
+}
+
+const STATS_INIT: [Stat; WhichStat::TotalStatCount as usize] = [
+    Stat::float("load_avg1"),
+    Stat::int("mem_total"),
+    Stat::int("mem_available"),
+    Stat::int("mem_active"),
+    Stat::int("mem_inactive"),
+    Stat::int("mem_anonhp"),
+];
+
+pub struct Stats {
+    in_range: bool,
+    rdbuf: String,
+    fp_loadavg: Option<File>,
+    fp_meminfo: Option<File>,
+    stats: [Stat; WhichStat::TotalStatCount as usize],
+}
+
+impl Stats {
+    pub const fn new() -> Self {
+        Self {
+            in_range: false,
+            rdbuf: String::new(),
+            fp_loadavg: None,
+            fp_meminfo: None,
+            stats: STATS_INIT,
+        }
+    }
+
+    pub fn begin_range(&mut self) {
+        self.in_range = true;
+        self.stats.iter_mut().for_each(Stat::reset);
+    }
+
+    pub fn end_range(&mut self) {
+        self.in_range = false;
+    }
+
+    pub fn stat_packet(&self) -> String {
+        let mut outbuf = String::with_capacity(1024);
+        for stat in self.stats.iter() {
+            let avg = stat.value_sum.valf() / stat.value_cnt.max(1) as f64;
+            write!(
+                &mut outbuf,
+                "{n},{now},{n}-min,{min},{n}-max,{max},{n}-avg,{avg},",
+                n = stat.name,
+                now = stat.value_now,
+                min = stat.value_min,
+                max = stat.value_max,
+                avg = avg,
+            )
+            .unwrap();
+        }
+        outbuf.pop();
+        outbuf.push('\n');
+        outbuf
+    }
+
+    pub fn update(&mut self) -> Result<()> {
+        if self.rdbuf.capacity() < 4096 {
+            self.rdbuf.reserve(4096);
+        }
+        self.upd_loadavg()?;
+        self.upd_meminfo()?;
+        if self.in_range {
+            self.stats.iter_mut().for_each(Stat::update_aggregate);
+        }
+        Ok(())
+    }
+
+    fn upd_loadavg(&mut self) -> Result<()> {
+        if self.fp_loadavg.is_none() {
+            self.fp_loadavg = Some(OpenOptions::new().read(true).open("/proc/loadavg")?);
+        }
+        let fp_loadavg = self.fp_loadavg.as_mut().unwrap();
+        self.rdbuf.clear();
+        fp_loadavg.read_to_string(&mut self.rdbuf)?;
+        fp_loadavg.seek(SeekFrom::Start(0))?;
+        let ldavg = self
+            .rdbuf
+            .split_once(' ')
+            .context("Splitting 1min loadavg")?
+            .0;
+        self.stats[WhichStat::LoadAvg1min as usize]
+            .value_now
+            .setf(ldavg.parse().context("Parsing 1min loadavg")?);
+        Ok(())
+    }
+
+    // line -> (var, bytes)
+    fn parse_meminfo_line(line: &str) -> Result<(&str, i64)> {
+        let (vname, mut vval) = line.split_once(':').context("Splitting meminfo line")?;
+        let mut multiplier = 1i64;
+        vval = vval.trim();
+        vval = match vval.strip_suffix("kB") {
+            Some(s) => {
+                multiplier *= 1024;
+                s.trim_end()
+            }
+            None => vval,
+        };
+        vval = match vval.strip_suffix("MB") {
+            Some(s) => {
+                multiplier *= 1024 * 1024;
+                s.trim_end()
+            }
+            None => vval,
+        };
+        vval = match vval.strip_suffix("GB") {
+            Some(s) => {
+                multiplier *= 1024 * 1024 * 1024;
+                s.trim_end()
+            }
+            None => vval,
+        };
+        vval = match vval.strip_suffix('B') {
+            Some(s) => s.trim_end(),
+            None => vval,
+        };
+        let vnum: i64 = vval.parse().context("Parsing meminfo value")?;
+        Ok((vname.trim(), vnum * multiplier))
+    }
+
+    fn upd_meminfo(&mut self) -> Result<()> {
+        if self.fp_meminfo.is_none() {
+            self.fp_meminfo = Some(OpenOptions::new().read(true).open("/proc/meminfo")?);
+        }
+        let fp_meminfo = self.fp_meminfo.as_mut().unwrap();
+        self.rdbuf.clear();
+        fp_meminfo.read_to_string(&mut self.rdbuf)?;
+        fp_meminfo.seek(SeekFrom::Start(0))?;
+        for line in self
+            .rdbuf
+            .lines()
+            .map(|l| l.trim())
+            .filter(|l| !l.is_empty())
+        {
+            let (var, bytes) = Self::parse_meminfo_line(line)?;
+            match var {
+                "MemTotal" => {
+                    self.stats[WhichStat::MemTotal as usize]
+                        .value_now
+                        .seti(bytes);
+                }
+                "MemAvailable" => {
+                    self.stats[WhichStat::MemAvailable as usize]
+                        .value_now
+                        .seti(bytes);
+                }
+                "Active" => {
+                    self.stats[WhichStat::MemActive as usize]
+                        .value_now
+                        .seti(bytes);
+                }
+                "Inactive" => {
+                    self.stats[WhichStat::MemInactive as usize]
+                        .value_now
+                        .seti(bytes);
+                }
+                "AnonHugePages" => {
+                    self.stats[WhichStat::MemAnonHugePages as usize]
+                        .value_now
+                        .seti(bytes);
+                }
+                _ => {}
+            }
+        }
+        Ok(())
+    }
+}
+
+impl Default for Stats {
+    fn default() -> Self {
+        Self::new()
+    }
+}
