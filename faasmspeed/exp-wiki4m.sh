dce0b8e674cff6313c134ccfc25a0f85d9f0cb62
Jakub Szewczyk <git@kubasz.xyz>
2021-09-21 21:09:29 +0100

    Updated experiments

--- faasmspeed/exp-wiki4m.sh
+++ faasmspeed/exp-wiki4m.sh
@@ -1,36 +1,35 @@
 #!/bin/sh
 
-OUTFILE=wresults-$(date -I).log
+DATASET=$(seq --format='wiki4m/frag_%03.0f' 0 255)
+
+OUTFILE=results-$(date -I).log
 
 echo Writing results to $OUTFILE
 printf "" > $OUTFILE
 
-RPS_LIST="5 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95 100 105 110 115 120 125 130 135 140"
+FS_ARGS="wordcount ${DATASET}"
+RPS_LIST="10 20 30 40 50 60 70 80 90 100 120 140 160 180 200 250 300 310 320 330 340 350 360 370 380 390 400 410 420 430 440 450 460 470 480 490 500"
+NDP_LIST="0.0 0.25 0.5 0.75 1.0"
 TIME_PER=10
 FAASMSPEED=faasmspeed
-PARALLELISM=1
+PARALLELISM=2
 WARMUP_TIME=2
 TIMEOUT=45000
 HOST=http://127.0.0.1:8080
 MONITOR_HOSTS='localhost:8125;luna:8125'
 #FAASMSPEED=echo
-DATASET=$(seq --format='wiki4m/frag_%03.0f' 0 255)
+
+for NDP in ${NDP_LIST}
+do
 
 for RPS in ${RPS_LIST}
 do
-    echo "*** Wordcount RPS=${RPS}"
+    echo "*** Wordcount NDP=${NDP} RPS=${RPS}"
     # Warm-up burst
-    ${FAASMSPEED} -h ${HOST} -u ndp -f wordcount -c -x ${TIMEOUT} -t ${WARMUP_TIME} -r ${RPS} -p ${PARALLELISM} ${DATASET} 2>&1 > /dev/null
+    ${FAASMSPEED} -h ${HOST} -u ndp -N ${NDP} -f ${FS_ARGS} -c -x ${TIMEOUT} -t ${WARMUP_TIME} -r ${RPS} -p ${PARALLELISM} 2>&1 > /dev/null
     sleep 0.2
-    ${FAASMSPEED} -h ${HOST} -u ndp -f wordcount -c -m ${MONITOR_HOSTS} -x ${TIMEOUT} -t ${TIME_PER} -r ${RPS} -p ${PARALLELISM} ${DATASET} >> $OUTFILE
+    ${FAASMSPEED} -h ${HOST} -u ndp -N ${NDP} -f ${FS_ARGS} -c -m ${MONITOR_HOSTS} -x ${TIMEOUT} -t ${TIME_PER} -r ${RPS} -p ${PARALLELISM} >> $OUTFILE
 done
-sleep 5
+sleep 35
 
-for RPS in ${RPS_LIST}
-do
-    echo "*** Wordcount-NDP RPS=${RPS}"
-    # Warm-up burst
-    ${FAASMSPEED} -h ${HOST} -u ndp -f wordcount -N -c -x ${TIMEOUT} -t ${WARMUP_TIME} -r ${RPS} -p ${PARALLELISM} ${DATASET} 2>&1 > /dev/null
-    sleep 0.2
-    ${FAASMSPEED} -h ${HOST} -u ndp -f wordcount -N -c -m ${MONITOR_HOSTS} -x ${TIMEOUT} -t ${TIME_PER} -r ${RPS} -p ${PARALLELISM} ${DATASET} >> $OUTFILE
 done
--- faasmspeed/experiments.sh
+++ faasmspeed/experiments.sh
@@ -5,7 +5,9 @@ OUTFILE=results-$(date -I).log
 echo Writing results to $OUTFILE
 printf "" > $OUTFILE
 
-RPS_LIST="1 10 20 30 40 50 60 70 80 90 100 120 140 160 180 200 250 300 310 320 330 340 350 360 370 380 390 400 410 420 430 440 450 460 470 480 490 500 520 540 560 600 700 800 900 1000"
+FS_ARGS="wordcount frankenmod.txt"
+RPS_LIST="10 20 30 40 50 60 70 80 90 100 120 140 160 180 200 250 300 310 320 330 340 350 360 370 380 390 400 410 420 430 440 450 460 470 480 490 500 520 540 560 600 700 800 900 1000 1100 1200 1300 1400 1500"
+NDP_LIST="0.0 0.25 0.5 0.75 1.0"
 TIME_PER=10
 FAASMSPEED=faasmspeed
 PARALLELISM=2
@@ -15,21 +17,17 @@ HOST=http://127.0.0.1:8080
 MONITOR_HOSTS='localhost:8125;luna:8125'
 #FAASMSPEED=echo
 
-for RPS in ${RPS_LIST}
+for NDP in ${NDP_LIST}
 do
-    echo "*** Wordcount RPS=${RPS}"
-    # Warm-up burst
-    ${FAASMSPEED} -h ${HOST} -u ndp -f wordcount -N frankenmod.txt -c -x ${TIMEOUT} -t ${WARMUP_TIME} -r ${RPS} -p ${PARALLELISM} 2>&1 > /dev/null
-    sleep 0.2
-    ${FAASMSPEED} -h ${HOST} -u ndp -f wordcount -N frankenmod.txt -c -m ${MONITOR_HOSTS} -x ${TIMEOUT} -t ${TIME_PER} -r ${RPS} -p ${PARALLELISM} >> $OUTFILE
-done
-sleep 35
 
 for RPS in ${RPS_LIST}
 do
-    echo "*** Wordcount-NDP RPS=${RPS}"
+    echo "*** Wordcount NDP=${NDP} RPS=${RPS}"
     # Warm-up burst
-    ${FAASMSPEED} -h ${HOST} -u ndp -f wordcount frankenmod.txt -c -x ${TIMEOUT} -t ${WARMUP_TIME} -r ${RPS} -p ${PARALLELISM} 2>&1 > /dev/null
+    ${FAASMSPEED} -h ${HOST} -u ndp -N ${NDP} -f ${FS_ARGS} -c -x ${TIMEOUT} -t ${WARMUP_TIME} -r ${RPS} -p ${PARALLELISM} 2>&1 > /dev/null
     sleep 0.2
-    ${FAASMSPEED} -h ${HOST} -u ndp -f wordcount frankenmod.txt -c -m ${MONITOR_HOSTS} -x ${TIMEOUT} -t ${TIME_PER} -r ${RPS} -p ${PARALLELISM} >> $OUTFILE
+    ${FAASMSPEED} -h ${HOST} -u ndp -N ${NDP} -f ${FS_ARGS} -c -m ${MONITOR_HOSTS} -x ${TIMEOUT} -t ${TIME_PER} -r ${RPS} -p ${PARALLELISM} >> $OUTFILE
+done
+sleep 35
+
 done
