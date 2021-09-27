ec3a3e786dbce00eb40e0c9ad00c9840a831f932
Jakub Szewczyk <git@kubasz.xyz>
2021-09-27 19:23:12 +0100

    Update wiki4m experiment

--- faasmspeed/exp-wiki4m.sh
+++ faasmspeed/exp-wiki4m.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-DATASET=$(seq --format='wiki4m/frag_%03.0f' 0 255)
+DATASET=$(seq --format='wiki4m/frag_%06.0f' 0 20274)
 
-OUTFILE=results-$(date -I).log
+OUTFILE=results-wiki4m-$(date -I).log
 
 echo Writing results to $OUTFILE
 printf "" > $OUTFILE
 
 FS_ARGS="wordcount ${DATASET}"
-RPS_LIST="10 20 30 40 50 60 70 80 90 100 120 140 160 180 200 250 300 310 320 330 340 350 360 370 380 390 400 410 420 430 440 450 460 470 480 490 500"
+RPS_LIST="$(seq 10 10 100) $(seq 100 25 750)"
 NDP_LIST="0 1 2 3 4"
 TIME_PER=10
 FAASMSPEED=faasmspeed
 PARALLELISM=2
 WARMUP_TIME=2
 TIMEOUT=45000
-HOST=http://127.0.0.1:8080
-MONITOR_HOSTS='localhost:8125;luna:8125'
+HOST=http://192.168.3.30:8080
+MONITOR_HOSTS='luna:8125;kone:8125'
 #FAASMSPEED=echo
 
 for NDP in ${NDP_LIST}
@@ -26,10 +26,12 @@ for RPS in ${RPS_LIST}
 do
     echo "*** Wordcount NDP=${NDP} RPS=${RPS}"
     # Warm-up burst
+    ${FAASMSPEED} -h ${HOST} -u ndp -n ${NDP} -N 4 -f ${FS_ARGS} -c -x ${TIMEOUT} -t 180000 -r ${RPS} -p ${PARALLELISM} -o 2>&1 > /dev/null
     ${FAASMSPEED} -h ${HOST} -u ndp -n ${NDP} -N 4 -f ${FS_ARGS} -c -x ${TIMEOUT} -t ${WARMUP_TIME} -r ${RPS} -p ${PARALLELISM} 2>&1 > /dev/null
-    sleep 0.2
+    ${FAASMSPEED} -h ${HOST} -u ndp -n ${NDP} -N 4 -f ${FS_ARGS} -c -x ${TIMEOUT} -t ${WARMUP_TIME} -r ${RPS} -p ${PARALLELISM} -o 2>&1 > /dev/null
+    sleep 1
     ${FAASMSPEED} -h ${HOST} -u ndp -n ${NDP} -N 4 -f ${FS_ARGS} -c -m ${MONITOR_HOSTS} -x ${TIMEOUT} -t ${TIME_PER} -r ${RPS} -p ${PARALLELISM} >> $OUTFILE
 done
-sleep 35
+sleep 60
 
 done
