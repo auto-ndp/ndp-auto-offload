e41e3b225d14dbe3203243fc1ea86efea617586a
Jakub Szewczyk <git@kubasz.xyz>
2021-10-01 13:54:00 +0100

    Forgot to update stublib header

--- pin-NearMAP/stublib/include/stublib.h
+++ pin-NearMAP/stublib/include/stublib.h
@@ -1,11 +1,14 @@
 #ifndef STUBLIB_PIN_NEARMAP_H_INCLUDED
 #define STUBLIB_PIN_NEARMAP_H_INCLUDED 1
 
+#include <stdint.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 void pinnearmap_phase(const char* name);
+void pinnearmap_io_bytes(uint64_t bytes);
 
 #ifdef __cplusplus
 }
