95c1752ca9c4b7c8bb1b0fa9d94da556eda07c91
Jakub Szewczyk <git@kubasz.xyz>
2022-01-10 13:32:31 +0000

    Update faasmspeed

--- faasmspeed/exp-wiki4m.sh
+++ faasmspeed/exp-wiki4m.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
-DATASET=$(seq --format='wiki4m/frag_%06.0f' 0 20274)
+seq --format='wiki4m/frag_%06.0f' 0 20274 > /tmp/wc_dataset
+DATASET="@/tmp/wc_dataset"
 
-OUTFILE=results-wiki4m-$(date -I).log
+OUTFILE=results-wc4m-$(date -I).log
 
 echo Writing results to $OUTFILE
 printf "" > $OUTFILE
 
 FS_ARGS="wordcount ${DATASET}"
 RPS_LIST="$(seq 10 10 90) $(seq 100 25 750)"
-NDP_LIST="0 1 2 3 4"
+NDP_LIST="0 1 2 3 4 5 6"
 TIME_PER=10
 FAASMSPEED=faasmspeed
-PARALLELISM=2
+PARALLELISM=4
 WARMUP_TIME=2
 TIMEOUT=45000
 HOST=http://192.168.3.30:8080
@@ -26,11 +27,11 @@ for RPS in ${RPS_LIST}
 do
     echo "*** Wordcount NDP=${NDP} RPS=${RPS}"
     # Warm-up burst
-    ${FAASMSPEED} -h ${HOST} -u ndp -n ${NDP} -N 4 -f ${FS_ARGS} -c -x ${TIMEOUT} -t 180000 -r ${RPS} -p ${PARALLELISM} -o > /dev/null 2>&1
-    ${FAASMSPEED} -h ${HOST} -u ndp -n ${NDP} -N 4 -f ${FS_ARGS} -c -x ${TIMEOUT} -t ${WARMUP_TIME} -r ${RPS} -p ${PARALLELISM} > /dev/null 2>&1
-    ${FAASMSPEED} -h ${HOST} -u ndp -n ${NDP} -N 4 -f ${FS_ARGS} -c -x ${TIMEOUT} -t ${WARMUP_TIME} -r ${RPS} -p ${PARALLELISM} -o > /dev/null 2>&1
+    ${FAASMSPEED} -h ${HOST} -u ndp -n ${NDP} -N 6 -f ${FS_ARGS} -c -x ${TIMEOUT} -t 180000 -r ${RPS} -p ${PARALLELISM} -o > /dev/null 2>&1
+    ${FAASMSPEED} -h ${HOST} -u ndp -n ${NDP} -N 6 -f ${FS_ARGS} -c -x ${TIMEOUT} -t ${WARMUP_TIME} -r ${RPS} -p ${PARALLELISM} > /dev/null 2>&1
+    ${FAASMSPEED} -h ${HOST} -u ndp -n ${NDP} -N 6 -f ${FS_ARGS} -c -x ${TIMEOUT} -t ${WARMUP_TIME} -r ${RPS} -p ${PARALLELISM} -o > /dev/null 2>&1
     sleep 1
-    ${FAASMSPEED} -h ${HOST} -u ndp -n ${NDP} -N 4 -f ${FS_ARGS} -c -m ${MONITOR_HOSTS} -x ${TIMEOUT} -t ${TIME_PER} -r ${RPS} -p ${PARALLELISM} >> $OUTFILE
+    ${FAASMSPEED} -h ${HOST} -u ndp -n ${NDP} -N 6 -f ${FS_ARGS} -c -m ${MONITOR_HOSTS} -x ${TIMEOUT} -t ${TIME_PER} -r ${RPS} -p ${PARALLELISM} >> $OUTFILE
 done
 sleep 60
 
