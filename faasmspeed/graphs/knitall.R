c47c345f2bee6aa383b44b61d228bf811d15ae79
Jakub Szewczyk <git@kubasz.xyz>
2022-01-12 12:13:19 +0000

    Visual tweaks, automation

--- faasmspeed/graphs/knitall.R
+++ faasmspeed/graphs/knitall.R
@@ -1,11 +1,16 @@
+#!/usr/bin/env Rscript
 library(rmarkdown)
 library(xfun)
 
-xfun::Rscript_call(
-    rmarkdown::render,
-    list(input = 'faasmspeed-param.Rmd',
-        output_format = 'html_document',
-        output_file = 'faasmspeed-wiki4M.html',
-        params = list(input_path = 'wtable.csv')
+tables = c('wtable', 'subtable', 'pcatable', 'thumbtable')
+
+for (table in tables) {
+    xfun::Rscript_call(
+        rmarkdown::render,
+        list(input = 'faasmspeed-param.Rmd',
+            output_format = 'html_document',
+            output_file = paste0('faasmspeed-', table, '.html'),
+            params = list(input_path = paste0(table, '.csv'))
+        )
     )
-)
+}
